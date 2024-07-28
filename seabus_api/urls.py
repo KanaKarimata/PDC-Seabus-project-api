d650f818860893cd88b7d3e9b6221019c3f98ad9
KanaKarimata <kana.lt22@gmail.com>
2024-07-28 19:55:25 +0900

    本番環境設定修正

--- seabus_api/urls.py
+++ seabus_api/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
@@ -13,4 +14,5 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('operation-rule/', include('operation_rules.urls')),
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
 ]
